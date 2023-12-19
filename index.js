be522122e5abae630facd1636ef8a7bad00d218f
vijayakumar58 <vijayalingam58@gmail.com>
2023-12-19 11:08:01 +0530

    add

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.listen(process.env.PORT || 3000, () => {
 app.use(express.json());
 app.use(cors({
     // origin:"http://localhost:3001",
-    origin:"https://vijaycrudformfrontend.netlify.app"
+    // origin:"https://vijaycrudformfrontend.netlify.app"
+    origin : "*"
 }))
 
 //welcome api
